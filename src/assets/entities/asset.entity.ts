db28996bb8cfbb6f94101ad63d39dc4922d6fcd6
Gabriel <paschoalfilho0303@gmail.com>
2025-02-18 17:43:48 -0300

    feature: create assets advice

--- src/assets/entities/asset.entity.ts
+++ src/assets/entities/asset.entity.ts
@@ -9,17 +9,17 @@ export class Asset {
   @Prop({ default: () => crypto.randomUUID() })
   _id: string;
 
-  @Prop()
+  @Prop({ unique: true, index: true })
   name: string;
 
-  @Prop()
+  @Prop({ unique: true, index: true })
   symbol: string;
 
   @Prop()
   image: string;
 
   @Prop()
-  price: number;
+  price: string;
 
   @Prop()
   createdAt!: Date;
