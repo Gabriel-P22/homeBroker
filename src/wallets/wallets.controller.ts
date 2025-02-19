0faf736096badb6f8e2747bd7ba59b43328a5327
Gabriel <paschoalfilho0303@gmail.com>
2025-02-19 11:00:12 -0300

    feature: create wallet asset

--- src/wallets/wallets.controller.ts
+++ src/wallets/wallets.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { CreateWalletAssetDto } from './dto/create-wallet-asset.dto';
 
 @Controller('wallets')
 export class WalletsController {
-  constructor(private readonly walletsService: WalletsService) {}
+  constructor(private readonly walletsService: WalletsService) { }
 
   @Post()
   create(@Body() createWalletDto: CreateWalletDto) {
@@ -19,16 +19,18 @@ export class WalletsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.walletsService.findOne(+id);
+    return this.walletsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto) {
-    return this.walletsService.update(+id, updateWalletDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.walletsService.remove(+id);
+  @Post(':id/assets')
+  createWalletAsset(
+    @Param('id') id: string,
+    @Body() data: CreateWalletAssetDto,
+  ) {
+    return this.walletsService.createWalletAsset({
+      walletId: id,
+      assetId: data.assetId,
+      shares: data.shares,
+    });
   }
 }
--- src/wallets/wallets.service.ts
+++ src/wallets/wallets.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Wallet } from './entities/wallet.entity';
+import { Model } from 'mongoose';
+import { WalletAsset } from './entities/wallet-asset.entity';
 
 @Injectable()
 export class WalletsService {
+  constructor(
+    @InjectModel(Wallet.name)
+    private walletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name)
+    private walletAssetSchema: Model<WalletAsset>,
+  ) { }
+
   create(createWalletDto: CreateWalletDto) {
-    return 'This action adds a new wallet';
+    return this.walletSchema.create(createWalletDto);
   }
 
   findAll() {
-    return `This action returns all wallets`;
+    return this.walletSchema.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wallet`;
+  findOne(id: string) {
+    return this.walletSchema.findById(id);
   }
 
-  update(id: number, updateWalletDto: UpdateWalletDto) {
-    return `This action updates a #${id} wallet`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} wallet`;
+  createWalletAsset(data: {
+    walletId: string;
+    assetId: string;
+    shares: number;
+  }) {
+    return this.walletAssetSchema.create({
+      wallet: data.walletId,
+      asset: data.assetId,
+      shares: data.shares,
+    });
   }
 }
